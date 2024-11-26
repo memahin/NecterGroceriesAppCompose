fcfe3bcb759b4d5aace117a0e0c4be16bd496ed6
memahin <me.mahin1@gmail.com>
2024-11-26 16:01:58 +0600

    screen

--- app/src/main/java/com/mahin/nectergroceriesapp_compose/MainActivity.kt
+++ app/src/main/java/com/mahin/nectergroceriesapp_compose/MainActivity.kt
@@ -19,29 +19,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NecterGroceriesAppComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     NecterGroceriesAppComposeTheme {
-        Greeting("Android")
     }
 }
